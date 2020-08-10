4dc8bc7ac6d297baa169423ee70690ebb8685c3c
rektdeckard <friedtm@gmail.com>
2020-08-09 23:43:18 -0400

    Footer: add 1280px breakpoint styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -23,56 +23,51 @@ const Footer: React.FC<FooterProps> = () => {
 
   return (
     <footer>
-      <div className="outro-container">
-        <div className="outro">
-          <h2>
-            Phosphor is free and open source software. If you enjoy these icons,
-            please consider supporting us with a donation.
-          </h2>
-          <button
-            className="main-button"
-            onClick={(e) => e.currentTarget.blur()}
+      <div className="outro">
+        <h2>
+          Phosphor is free and open source software. If you enjoy these icons,
+          please support us with a donation.
+        </h2>
+        <button className="main-button" onClick={(e) => e.currentTarget.blur()}>
+          <Heart size={24} weight="fill" style={{ marginRight: 12 }} />
+          Buy us a coffee
+        </button>
+        <p className="fine-print">
+          Copyright © 2020 Phosphor Icons
+          <br />
+          Designed by{" "}
+          <a className="main-link" href="https://helenazhang.com">
+            Helena Zhang
+          </a>{" "}
+          + built by{" "}
+          <a className="main-link" href="https://tobiasfried.com">
+            Tobias Fried
+          </a>{" "}
+          <span
+            role="img"
+            aria-label="Emoji of woman technologist, man technologist, and cat"
           >
-            <Heart size={24} weight="fill" style={{ marginRight: 12 }} />
-            Buy us a coffee
-          </button>
-          <p className="fine-print">
-            Copyright © 2020 Phosphor Icons
-            <br />
-            Designed by{" "}
-            <a className="main-link" href="https://helenazhang.com">
-              Helena Zhang
-            </a>{" "}
-            + built by{" "}
-            <a className="main-link" href="https://tobiasfried.com">
-              Tobias Fried
-            </a>{" "}
-            <span
-              role="img"
-              aria-label="Emoji of woman technologist, man technologist, and cat"
-            >
-              👩🏻‍💻👨‍💻🐱
-            </span>
-            <br />
-            This website is set in{" "}
-            <a className="main-link" href="https://manropefont.com/">
-              Manrope
-            </a>{" "}
-            by{" "}
-            <a className="main-link" href="https://gent.media/">
-              Mikhail Sharanda
-            </a>
-            .
-            <br />
-            See also:{" "}
-            <a
-              className="main-link"
-              href="https://play.google.com/store/apps/details?id=com.tobiasfried.phosphor"
-            >
-              Phosphor for Android
-            </a>
-          </p>
-        </div>
+            👩🏻‍💻👨‍💻🐱
+          </span>
+          <br />
+          This website is set in{" "}
+          <a className="main-link" href="https://manropefont.com/">
+            Manrope
+          </a>{" "}
+          by{" "}
+          <a className="main-link" href="https://gent.media/">
+            Mikhail Sharanda
+          </a>
+          .
+          <br />
+          See also:{" "}
+          <a
+            className="main-link"
+            href="https://play.google.com/store/apps/details?id=com.tobiasfried.phosphor"
+          >
+            Phosphor for Android
+          </a>
+        </p>
         <div id="back-to-top">
           <button
             id="back-to-top-button"
