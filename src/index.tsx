496c14c8c65db401cfac34b28e34382d414fc69c
rektdeckard <friedtm@gmail.com>
2020-10-10 01:15:35 -0400

    meta: replace Google Analytics with react-ga

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
 import * as serviceWorker from "./serviceWorker";
 import App from "./components/App/App";
-import { RecoilRoot } from "recoil";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("UA-179205759-1", { titleCase: false });
+ReactGA.pageview(window.location.pathname);
 
 ReactDOM.render(
   <React.StrictMode>
