a9ce9faac825281bbb22e227ad384897f2c13daa
rektdeckard <friedtm@gmail.com>
2022-05-08 14:24:20 -0600

    chore(app): add vote analytics

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { Medal } from "phosphor-react"; 
+import { Medal } from "phosphor-react";
+import ReactGA from "react-ga";
 
 import "./Banner.css";
 
 const Banner = () => {
+  const handleClick = () => {
+    ReactGA.event({ category: "Outbound", action: "Click", label: "Vote" });
+    window.open(
+      "https://www.figma.com/community/file/903830135544202908",
+      "_blank",
+      "noopener noreferrer"
+    );
+  };
+
   return (
     <div className="banner">
-       The 2022 Figma Community Awards are here!
-      <button
-        className="main-button"
-        onClick={
-            () => window.open(
-              "https://www.figma.com/community/file/903830135544202908",
-              "_blank",
-              "noopener noreferrer",
-            )
-        }>
-          <Medal size={24} weight="fill" />
-          Vote for Phosphor
-        </button>
+      The 2022 Figma Community Awards are here!
+      <button className="main-button" onClick={handleClick}>
+        <Medal size={24} weight="fill" />
+        Vote for Phosphor
+      </button>
     </div>
   );
 };
