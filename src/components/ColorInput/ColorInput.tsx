2fc3263cdc64456553ee47da13e7906df4088db2
rektdeckard <friedtm@gmail.com>
2023-04-20 21:23:14 -0600

    feat(app): add currentcolor picker option

--- src/components/ColorInput/ColorInput.tsx
+++ src/components/ColorInput/ColorInput.tsx
@@ -1,5 +1,6 @@
 import { useCallback } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
+import { EyedropperSample } from "@phosphor-icons/react";
 
 import { useThrottled } from "@/hooks";
 import { iconColorAtom, isDarkThemeSelector } from "@/state";
@@ -22,12 +23,21 @@ const ColorInput = (_: ColorInputProps) => {
     [setColor]
   );
 
+  const handleSetInheritedColor = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setColor("currentColor");
+  };
+
   const throttledColorChange = useThrottled(handleColorChange, 100, [
     handleColorChange,
   ]);
 
   return (
-    <div className="color-picker">
+    <div
+      className="color-picker"
+      title="Select a color, or right-click to use inherited color"
+      onContextMenu={handleSetInheritedColor}
+    >
       <input
         className="color-input"
         aria-label="Icon Color"
@@ -36,7 +46,13 @@ const ColorInput = (_: ColorInputProps) => {
         onChange={throttledColorChange}
         value={color}
       />
-      <span style={{ color: isDark ? "black" : "white" }}>{color}</span>
+      <span style={{ color: isDark ? "black" : "white" }}>
+        {color === "currentColor" ? (
+          <EyedropperSample size={28} weight="fill" />
+        ) : (
+          color
+        )}
+      </span>
     </div>
   );
 };
